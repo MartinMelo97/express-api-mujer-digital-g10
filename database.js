769944a4ae886cf8bc9bf4739f103fa666564f4c
Martin Melo <martin.melo.dev.97@gmail.com>
2024-02-08 14:00:56 -0600

    Primeros endpoints

---
+++ database.js
@@ -0,0 +1,33 @@
+const products = [
+    {
+        id: 1,
+        nombre: 'Chips Tradicionales',
+        precio: 20,
+        descripcion: 'Tan ricas',
+        cantidadDisponible: 10,
+        marca: 'Barcel',
+        categoria: 'Frituras',
+    },
+    {
+        id: 2,
+        nombre: 'Gansito',
+        precio: 25,
+        cantidadDisponible: 5,
+        descripcion: 'Nunca me gustaron',
+        marca: 'Marinela',
+        categoria: 'Pan',
+    },
+    {
+        id: 3,
+        nombre: 'Galletas Emperador Chocolate',
+        precio: 20,
+        descripcion: 'GUARDIAS',
+        cantidadDisponible: 30,
+        marca: 'Gamesa',
+        categoria: 'Galletas',
+    },
+]
+
+module.exports = {
+    products
+}
--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const { products } = require('./database');
+
+dotenv.config();
+
 const app = express();
 
-app.get('/', function(req, res) {
-    return res.status(200).json({ message: 'Hola Mundo! MD' });
+app.get('/products', function(req, res) {
+    return res.status(200).json(products);
 });
 
-app.listen(8000, function() {
-    console.log('La API está corriendo en el puerto 8000');
+app.get('/products/:id', function(req, res) {
+    const { id } = req.params;
+    const product = products.find(function (product) {
+        return product.id === +id;
+    });
+
+    if (!product) {
+        return res.status(404).json({
+            message: 'El producto solicitado no existe'
+        })
+    }
+
+    return res.status(200).json(product)
+});
+
+
+app.get('/', function(req, res) {
+    return res.status(200).json({ message: 'Hola Mundo! Mujer Digital' });
+});
+
+const port = process.env.PORT;
+
+app.listen(port, function() {
+    console.log(`La API está corriendo en el puerto ${port}`);
 });
